df31a33c7dd78111b4646692df3554295eeccffe
wei_lin <wei1234c@gmail.com>
2019-05-17 15:50:25 +0800

    enhance topolograph generation.

--- codes/test/test.py
+++ codes/test/test.py
@@ -1,6 +1,4 @@
-from universal_serial_bus.orm.usb20 import *
-from array import array
-
-desp = array('B', [18, 1, 16, 1, 0, 0, 0, 8, 63, 27, 8, 32, 0, 1, 1, 2, 0, 1])
-dev_dscrpt = StandardDeviceDescriptor.from_byte_array(desp)
-print(dev_dscrpt)
+import networkx as nx
+G = nx.MultiDiGraph()
+n = G.add_node(1,width = 1000)
+print(nx.nodes(G))
